99350f6c3d04aa6d6c35dfee12116d64fd610e58
Diego Lima <xxdiegolsxx@gmail.com>
2022-10-17 16:15:34 -0300

    protocol page

--- src/pages/Protocol.jsx
+++ src/pages/Protocol.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+import { theme } from '../utils/theme';
+
+const Container = styled.div`
+  background-color: ${theme.colors.secondaryBlue};
+  margin: 0 -0.5rem -0.5rem -0.5rem;
+  min-height: 87vh;
+`;
 
 const ProtocolPage = () => {
-  return (
-    <div>
-      <p>oi</p>
-    </div>
-  );
+  return <Container>{/* <p>oi</p> */}</Container>;
 };
 
 export default ProtocolPage;

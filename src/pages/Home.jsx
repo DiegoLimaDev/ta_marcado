3237a610d8cfcb3d488b1a9b9bccc977efcce840
Diego Lima <xxdiegolsxx@gmail.com>
2022-10-18 13:09:47 -0300

    adptation to routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,25 +4,13 @@ import styled from 'styled-components';
 
 import Topbar from '../components/Topbar';
 import Footer from '../components/Footer';
-import { useContext } from 'react';
-import { GlobalContext } from '../utils/GlobalContext';
 import InitialPage from './Initial';
-import ProtocolPage from './Protocol';
-import SchedulingPage from './Scheduling';
 
 const Home = () => {
-  const [page, setPage] = useContext(GlobalContext);
-
   return (
     <div>
       <Topbar />
-      {page === 'Home' ? (
-        <InitialPage />
-      ) : page === 'Protocol' ? (
-        <ProtocolPage />
-      ) : page === 'schedulingPage' ? (
-        <SchedulingPage />
-      ) : null}
+      <InitialPage />
       <Footer />
     </div>
   );

b86be901d4c0479df5b9c4775f2e78e972c7f77d
Diego Lima <xxdiegolsxx@gmail.com>
2022-10-29 16:15:37 -0300

    topbar and footer added to root

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 import { theme } from './utils/theme';
 import Context from './utils/GlobalContext';
 import SchedulingPage from './pages/Scheduling';
+import Topbar from './components/Topbar';
+import Footer from './components/Footer';
 
 const Container = styled.div`
   font-family: ${theme.font};
@@ -17,10 +19,12 @@ function App() {
     <Container>
       <Context>
         <BrowserRouter>
+          <Topbar />
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/schedulingPage" element={<SchedulingPage />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </Context>
     </Container>

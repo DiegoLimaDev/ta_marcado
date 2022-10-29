7e5b8940ebfc35fa57f26da5c356008d021596a0
Diego Lima <xxdiegolsxx@gmail.com>
2022-10-29 16:14:52 -0300

    separation of SchedulForm

--- src/pages/Scheduling.jsx
+++ src/pages/Scheduling.jsx
@@ -3,10 +3,8 @@ import styled, { css } from 'styled-components';
 import { theme } from '../utils/theme';
 
 import diary from '../assets/images/diary.svg';
-import { useState } from 'react';
-import Topbar from '../components/Topbar';
-import Footer from '../components/Footer';
 import { Button } from '../components/Buttons';
+import { ScheduleForm } from '../components/ScheduleForm';
 
 const Container = styled.div`
   background-color: ${theme.colors.secondaryBlue};
@@ -21,21 +19,6 @@ const Row = styled.div`
   justify-content: space-around;
   align-items: center;
   padding: 2rem;
-`;
-
-const InsideRow = styled.div`
-  ${({ checkbox }) => css`
-    direction: ltr;
-    display: flex;
-    width: 100%;
-    justify-content: ${checkbox === 'checkbox' ? 'left' : 'space-between'};
-  `}
-`;
-
-const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 40%;
 `;
 
 const Image = styled.img`
@@ -68,22 +51,6 @@ const Text = styled.p`
   `}
 `;
 
-const Forms = styled.form`
-  width: auto;
-  padding: 1rem;
-`;
-
-const Input = styled.input`
-  ${({ width }) => css`
-    background-color: ${theme.colors.auxiliar};
-    border: 0.5px solid;
-    border-color: ${theme.colors.darkBlue};
-    border-radius: 20px;
-    width: ${width}%;
-    padding: 0.2rem;
-  `}
-`;
-
 const ButtonDiv = styled.div`
   display: block;
   margin: 0 auto;
@@ -91,18 +58,8 @@ const ButtonDiv = styled.div`
 `;
 
 const SchedulingPage = () => {
-  const [remote, setRemote] = useState('');
-  const [reason, setReason] = useState('');
-  const [value, setValue] = useState('');
-
-  const handlechange = (e) => {
-    const result = e.target.value.replace(/\D/g, '');
-    setValue(result);
-  };
-
   return (
     <>
-      <Topbar />
       <Container>
         <Row>
           <Image src={diary} />
@@ -112,129 +69,7 @@ const SchedulingPage = () => {
                 FORMULÁRIO DE AGENDAMENTO
               </Text>
             </GradientBox>
-            <Forms>
-              <Text>NOME COMPLETO *</Text>
-              <Input type="text" placeholder="Nome" label="Nome" width="100" />
-              <Text>EMAIL *</Text>
-              <Input
-                type="text"
-                placeholder="Email"
-                label="email"
-                width="100"
-              />
-              <InsideRow>
-                <Column>
-                  <Text>CPF *</Text>
-                  <Input
-                    type="text"
-                    placeholder="CPF"
-                    label="CPF"
-                    width="100"
-                    value={value}
-                    onChange={handlechange}
-                  />
-                </Column>
-                <Column>
-                  <Text>TELEFONE *</Text>
-                  <Input
-                    type="text"
-                    placeholder="Telefone"
-                    label="Telefone"
-                    width="100"
-                    value={value}
-                    onChange={handlechange}
-                  />
-                </Column>
-              </InsideRow>
-              <Text>TIPO DE ATENDIMENTO *</Text>
-              <InsideRow checkbox="checkbox">
-                <Input
-                  type="checkbox"
-                  checked={remote === 'remote' ? true : false}
-                  onClick={() =>
-                    remote !== 'remote' ? setRemote('remote') : setRemote('')
-                  }
-                />
-                <Text>Remoto</Text>
-              </InsideRow>
-              <InsideRow checkbox="checkbox">
-                <Input
-                  type="checkbox"
-                  checked={remote === 'presential' ? true : false}
-                  onClick={() =>
-                    remote !== 'presential'
-                      ? setRemote('presential')
-                      : setRemote('')
-                  }
-                />
-                <Text>Presencial</Text>
-              </InsideRow>
-              <InsideRow>
-                <Column>
-                  <Text>DATA *</Text>
-                  <Input placeholder="Data" />
-                </Column>
-                <Column>
-                  <Text>HORÁRIO *</Text>
-                  <Input placeholder="Horário" />
-                </Column>
-              </InsideRow>
-              <Text>MOTIVO DO ATENDIMENTO *</Text>
-              <InsideRow checkbox="checkbox">
-                <Input
-                  type="checkbox"
-                  checked={reason === 'declarations' ? true : false}
-                  onClick={() =>
-                    reason !== 'declarations'
-                      ? setReason('declarations')
-                      : setReason('')
-                  }
-                />
-                <Text>Declarações</Text>
-              </InsideRow>
-              <InsideRow checkbox="checkbox">
-                <Input
-                  type="checkbox"
-                  checked={reason === 'vem' ? true : false}
-                  onClick={() =>
-                    reason !== 'vem' ? setReason('vem') : setReason('')
-                  }
-                />
-                <Text>VEM</Text>
-              </InsideRow>
-              <InsideRow checkbox="checkbox">
-                <Input
-                  type="checkbox"
-                  checked={reason === 'studentID' ? true : false}
-                  onClick={() =>
-                    reason !== 'studentID'
-                      ? setReason('studentID')
-                      : setReason('')
-                  }
-                />
-                <Text>Carteria de Estudante</Text>
-              </InsideRow>
-              <InsideRow checkbox="checkbox">
-                <Input
-                  type="checkbox"
-                  checked={reason === 'siepe' ? true : false}
-                  onClick={() =>
-                    reason !== 'siepe' ? setReason('siepe') : setReason('')
-                  }
-                />
-                <Text>SIEPE</Text>
-              </InsideRow>
-              <InsideRow checkbox="checkbox">
-                <Input
-                  type="checkbox"
-                  checked={reason === 'others' ? true : false}
-                  onClick={() =>
-                    reason !== 'others' ? setReason('others') : setReason('')
-                  }
-                />
-                <Text>Outros</Text>
-              </InsideRow>
-            </Forms>
+            <ScheduleForm />
             <ButtonDiv>
               <Button
                 width="40"
@@ -254,7 +89,6 @@ const SchedulingPage = () => {
           </Box>
         </Row>
       </Container>
-      <Footer />
     </>
   );
 };

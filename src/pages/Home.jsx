40999e253d4692221d1f7fb358292474b37675b6
Diego Lima <xxdiegolsxx@gmail.com>
2022-10-29 16:15:20 -0300

    initial page separation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { theme } from '../utils/theme';
-import styled from 'styled-components';
 
-import Topbar from '../components/Topbar';
-import Footer from '../components/Footer';
 import InitialPage from './Initial';
 
 const Home = () => {
   return (
     <div>
-      <Topbar />
       <InitialPage />
-      <Footer />
     </div>
   );
 };

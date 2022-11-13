4936e0a6b0ca1bee5bde812db1bc5b90b20fa6ae
Diego Lima <xxdiegolsxx@gmail.com>
2022-11-12 22:57:56 -0300

    MUI drawer

--- src/components/TopbarMobile.jsx
+++ src/components/TopbarMobile.jsx
@@ -6,6 +6,8 @@ import { theme } from '../utils/theme';
 
 import logo from '../assets/images/logo.jpeg';
 import logopd from '../assets/images/logo_pd.svg';
+import { Drawer } from '@mui/material';
+import { Css } from '@mui/icons-material';
 
 const MenuVisible = () => css`
   visibility: visible;
@@ -24,28 +26,34 @@ const MenuInvisible = () => css`
 `;
 
 const BoxMobile = styled.div`
-  ${({ visible }) => css`
+  ${() => css`
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    z-index: 5;
-    ${visible === true ? MenuVisible() : MenuInvisible()};
-    transition: transform 300ms cubic-bezier(0, 0.52, 0, 1);
-    z-index: 1000;
+    align-self: center;
+    background-color: ${theme.colors.lightBlue};
+    height: 100%;
+    width: 100%;
+    padding: 0 2rem;
+    margin-left: 1rem;
 
     .LinkDecoration {
       text-decoration: none;
+    }
+
+    .icon {
+      align-self: flex-start;
+      padding-top: 1rem;
     }
   `}
 `;
 
 const Links = styled.p`
   color: ${theme.colors.darkBlue};
-  font-size: ${theme.sizes.size2};
+  font-size: ${theme.sizes.size1};
   font-weight: bold;
   width: auto;
   cursor: pointer;
+  margin-left: 0.5rem;
 
   &:hover {
     transform: scale(1.2);
@@ -72,6 +80,8 @@ const Links = styled.p`
 
 const Row = styled.div`
   flex-direction: Row;
+  display: flex;
+  align-items: center;
 `;
 
 const Images = styled.img`
@@ -94,22 +104,15 @@ const IconContainer = styled.div`
   }
 `;
 
+const MyDrawer = styled(Drawer)(() => css``);
+
 export const TopbarMobile = () => {
   const [visible, setVisible] = useState(false);
 
   return (
     <>
       <IconContainer>
-        {visible === true ? (
-          <Icon
-            icon="ep:close"
-            width={40}
-            height={40}
-            className="icon"
-            onClick={() => setVisible(false)}
-            color={theme.colors.darkBlue}
-          />
-        ) : (
+        {visible === true ? null : (
           <Icon
             icon="eva:menu-outline"
             width={40}
@@ -120,30 +123,142 @@ export const TopbarMobile = () => {
           />
         )}
       </IconContainer>
-      <BoxMobile visible={visible}>
-        <Row>
-          <Images src={logo} />
-          <Images src={logopd} />
-        </Row>
-        <Link
-          to="/"
-          className="LinkDecoration"
-          onClick={() => setVisible(false)}
-          role="button"
-        >
+      <MyDrawer anchor="left" open={visible}>
+        <BoxMobile>
+          <Icon
+            icon="ep:close"
+            width={40}
+            height={40}
+            className="icon"
+            onClick={() => setVisible(false)}
+            color={theme.colors.darkBlue}
+          />
+          {/* <Row>
+            <Images src={logo} />
+            <Images src={logopd} />
+          </Row> */}
+          <Row>
+            <Icon
+              icon="ant-design:home-outlined"
+              width={20}
+              height={20}
+              color={theme.colors.darkBlue}
+            />
+            <Link
+              to="/"
+              className="LinkDecoration"
+              onClick={() => setVisible(false)}
+              role="button"
+            >
+              <Links>HOME</Links>
+            </Link>
+          </Row>
+          <Row>
+            <Icon
+              icon="ant-design:search-outlined"
+              width={20}
+              height={20}
+              color={theme.colors.darkBlue}
+            />
+
+            <Link
+              to="/checkProtocol"
+              className="LinkDecoration"
+              onClick={() => setVisible(false)}
+              role="button"
+            >
+              <Links>PROTOCOLO</Links>
+            </Link>
+          </Row>
           <Links>HOME</Links>
-        </Link>
-        <Link
-          to="/checkProtocol"
-          className="LinkDecoration"
-          onClick={() => setVisible(false)}
-          role="button"
-        >
-          <Links>PROTOCOLO</Links>
-        </Link>
-        <Links>HOME</Links>
-        <Links>SOBRE O PROJETO</Links>
-      </BoxMobile>
+          <Links>SOBRE O PROJETO</Links>
+        </BoxMobile>
+      </MyDrawer>
     </>
   );
 };
+
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import Drawer from '@mui/material/Drawer';
+// import Button from '@mui/material/Button';
+// import List from '@mui/material/List';
+// import Divider from '@mui/material/Divider';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import MailIcon from '@mui/icons-material/Mail';
+
+// export const TopbarMobile = () => {
+//   const [state, setState] = React.useState({
+//     top: false,
+//     left: false,
+//     bottom: false,
+//     right: false,
+//   });
+
+//   const toggleDrawer = (anchor, open) => (event) => {
+//     if (
+//       event.type === 'keydown' &&
+//       (event.key === 'Tab' || event.key === 'Shift')
+//     ) {
+//       return;
+//     }
+
+//     setState({ ...state, [anchor]: open });
+//   };
+
+//   const list = (anchor) => (
+//     <Box
+//       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+//       role="presentation"
+//       onClick={toggleDrawer(anchor, false)}
+//       onKeyDown={toggleDrawer(anchor, false)}
+//     >
+//       <List>
+//         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+//           <ListItem key={text} disablePadding>
+//             <ListItemButton>
+//               <ListItemIcon>
+//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+//               </ListItemIcon>
+//               <ListItemText primary={text} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//       <Divider />
+//       <List>
+//         {['All mail', 'Trash', 'Spam'].map((text, index) => (
+//           <ListItem key={text} disablePadding>
+//             <ListItemButton>
+//               <ListItemIcon>
+//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+//               </ListItemIcon>
+//               <ListItemText primary={text} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </Box>
+//   );
+
+//   return (
+//     <div>
+//       {['left', 'right', 'top', 'bottom'].map((anchor) => (
+//         <React.Fragment key={anchor}>
+//           <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+//           <Drawer
+//             anchor={anchor}
+//             open={state[anchor]}
+//             onClose={toggleDrawer(anchor, false)}
+//           >
+//             {list(anchor)}
+//           </Drawer>
+//         </React.Fragment>
+//       ))}
+//     </div>
+//   );
+// };
